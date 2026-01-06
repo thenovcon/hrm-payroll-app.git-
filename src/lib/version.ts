cab46b0dcc752bb2751425248cfa8105ca4a08cd
Koku Gborglah <kokug@Kokus-Mac-mini.local>
2026-01-06 13:46:43 -0500

    chore: bump version to 1.3.16

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.3.15';
-export const REVISION = 118;
-export const BUILD_TIMESTAMP = '2026-01-06 09:10'; // UTC
+export const APP_VERSION = '1.3.16';
+export const REVISION = 119;
+export const BUILD_TIMESTAMP = '2026-01-06 13:55'; // UTC
 export const APP_FULL_VERSION = `v${APP_VERSION} (Rev ${REVISION})`;
