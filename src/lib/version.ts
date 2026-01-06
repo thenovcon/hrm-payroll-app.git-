ae62c2c3efc19deda6a07730581d8d4be222a791
Koku Gborglah <kokug@Kokus-Mac-mini.local>
2026-01-05 23:06:46 -0500

    fix: Finalize sequential import logic (v1.3.6)

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.3.5';
-export const REVISION = 108;
+export const APP_VERSION = '1.3.6';
+export const REVISION = 109;
 export const BUILD_TIMESTAMP = '2026-01-05 15:00'; // UTC
 export const APP_FULL_VERSION = `v${APP_VERSION} (Rev ${REVISION})`;
