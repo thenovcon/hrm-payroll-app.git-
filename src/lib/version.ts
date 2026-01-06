588a02472f86c02726edd093c6d06d330b0824ac
Koku Gborglah <kokug@Kokus-Mac-mini.local>
2026-01-06 03:25:14 -0500

    chore: Bump version to v1.3.10 (Direct API Release)

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.3.9';
-export const REVISION = 112;
-export const BUILD_TIMESTAMP = '2026-01-05 15:00'; // UTC
+export const APP_VERSION = '1.3.10';
+export const REVISION = 113;
+export const BUILD_TIMESTAMP = '2026-01-06 03:00'; // UTC
 export const APP_FULL_VERSION = `v${APP_VERSION} (Rev ${REVISION})`;
